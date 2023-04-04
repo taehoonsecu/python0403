c4e4c059d3db2010dcc74bfa4ac41bcd5481681e
taehoon <2ht93@naver.com@outlook.kr>
2023-04-04 11:09:15 +0900

    클래스연습2

--- Person2.py
+++ Person2.py
@@ -1,24 +1,20 @@
-# class1.py
-#1) 클래스형식 정의
+# Person2.py 
 class Person:
-    #초기화메소드
+    #클래스 멤버 변수
+    num_person = 0 
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1 
     def print(self):
         print("My name is {0}".format(self.name))
 
-#2)인스턴스 생성
+
 p1 = Person()
 p2 = Person()
-p2.name="전우치"
-
-#3)메서드 호출
-p1.print()
-p2.print()
-
-#런타임에 변수추가
+print("숫자:{0}".format(Person.num_person))
 Person.title = "new title"
 print(p1.title)
 print(p2.title)
 print(Person.title)
+
 
---
+++ self누락_GString클래스.py
@@ -0,0 +1,15 @@
+#전역변수
+
+str = "Not Class Member"
+class GString:
+    def __init__(self):
+        #인스턴스 멤버변수
+        self.str = "" 
+    def set(self, msg):
+        self.str = msg
+    def print(self):
+        print(self.str)
+
+g = GString()
+g.set("First Message")
+g.print()
