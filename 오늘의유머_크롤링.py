8d17c634b12635599c64a54b19062dbfcb79165f
taehoon <2ht93@naver.com@outlook.kr>
2023-04-06 10:37:27 +0900

    웹크롤링연습3(우회)

--- 오늘의유머_크롤링.py
+++ 오늘의유머_크롤링.py
@@ -11,10 +11,14 @@ import re
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
 #파일에 저장 (r: raw string notation -> \\이렇게 안써도됨)
-f = open(r"c:\work\clien.txt","wt",encoding="utf-8")
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+#a+ : apeend ( 기존 컨텐츠위에 추가, read, write, a+)
+f = open(r"c:\work\todayHumor.txt","a+",encoding="utf-8")
+
+#현재는 1~10페이지
+for n in range(1,11):
+        #오유주소
+        data ='https://www.todayhumor.co.kr/board/list.php?'\
+            + 'table=bestofbest&page=' + str(n)
         print(data)
 
         #웹브라우져 헤더 추가 
@@ -23,23 +27,25 @@ for n in range(0,10):
 
         #한글이 깨지는 경우는 유니코드로 다시 해석
         page = data.decode('utf-8', 'ignore')
-        soup = BeautifulSoup(page, 'html.parser')
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
 
-        # <span class="subject_fixed" data-role="list-title-text" title="맥스튜디오 기본형 판매합니다.">
-	# 		맥스튜디오 기본형 판매합니다.
-	# </span>
+        soup = BeautifulSoup(page, 'html.parser')
+        list = soup.find_all('td', attrs={'class':'subject'})
+
+#    <td class="subject">
+# <a href="/board/view.php?table=bestofbest> [제목] ~
+# 	# </span>
 
         for item in list:
                 try:
-                        title = item.text
+                        title = item.find('a').text.strip()
+                        #print(title)
                         #print(title.strip())
                         
                         #아이폰 내용만있으면 출력
-                        if (re.search('아이폰', title)):
+                        if (re.search('한국', title)):
                                 print(title.strip())
                                 f.write(title.strip()+"\n")
-                        #         print('https://www.clien.net'  + item['href'])
+                        # print('https://www.clien.net'  + item['href'])
                 except:
                         pass
 
